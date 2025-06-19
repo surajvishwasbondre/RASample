1594dd672c344c59a01b1b986bd9d9ccb5f01e87
surajvishwasbondre <114866975+surajvishwasbondre@users.noreply.github.com>
2025-06-19 13:04:58 +0530

    Update XPathParserDemo.java

--- src/test/java/com/tests/XPathParserDemo.java
+++ src/test/java/com/tests/XPathParserDemo.java
@@ -22,7 +22,7 @@ import org.xml.sax.SAXException;
 public class XPathParserDemo {
    
    public static void main(String[] args) {
-	   System.setProperty("webdriver.chrome.driver", "D:\\Eclipse Workplace\\RAWebsite\\drivers\\chromedriver.exe");
+	   System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
        WebDriver driver = new ChromeDriver();
        driver.manage().window().maximize();
        driver.get("https://realtyassistant.in/sitemap.xml");
