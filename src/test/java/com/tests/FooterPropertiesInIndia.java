9e1211dd92afbfd467c5b6e0fb7ebb72100c887a
surajvishwasbondre <114866975+surajvishwasbondre@users.noreply.github.com>
2025-06-19 12:57:59 +0530

    Update FooterPropertiesInIndia.java

--- src/test/java/com/tests/FooterPropertiesInIndia.java
+++ src/test/java/com/tests/FooterPropertiesInIndia.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FooterPropertiesInIndia {
 
 	public static void main(String[] args) throws IOException {
-		System.setProperty("webdriver.chrome.driver", "D:\\Eclipse Workplace\\RAWebsite\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://realtyassistant.in/sitemap.xml"); 
